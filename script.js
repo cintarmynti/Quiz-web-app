127be6ea48debd099db9a6b1476e91efae9da929
Cinta Ramayanti <58018808+cintarmynti@users.noreply.github.com>
2023-02-11 09:42:07 +0700

    five commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let questions = [{
-    question: "Gurita, cumi-cumi, penyu, paus adalah contoh binatang yang hidup di ...",
+    question: "1. Gurita, cumi-cumi, penyu, paus adalah contoh binatang yang hidup di ...",
     answers: [{
         option: "air tawar",
         answer: false,
@@ -19,7 +19,7 @@ let questions = [{
     ],
 },
 {
-    question: "Manakah yang termasuk ciri-ciri binatang melata?",
+    question: "2. Manakah yang termasuk ciri-ciri binatang melata?",
     answers: [
         {
             option: "Termasuk kelompok hewan berdarah dingin",
@@ -40,7 +40,7 @@ let questions = [{
     ],
 },
 {
-    question: "Manakah yang termasuk binatang mamalia?",
+    question: "3. Manakah yang termasuk binatang mamalia?",
     answers: [
         {
             option: "kura-kura",
@@ -69,12 +69,12 @@ let no = 0;
 
 window.onload = function () {
     document.getElementById("pertanyaan").innerHTML = questions[0].question;
-    document.getElementById("nomer").innerHTML = no;
+    // document.getElementById("nomer").innerHTML = no;
 
     let text = "";
    
     for(let j=0; j<4; j++){
-        let pilihanJawaban = '<button onClick="cekJawaban('+j+')" id="pilihan'+ j +'" value="'+ questions[no].answers[j].answer+'">' + questions[no].answers[j].option + '</button>';
+        let pilihanJawaban = '<button class="w-100 p-3 btn-pil" onClick="cekJawaban('+j+')" id="pilihan'+ j +'" value="'+ questions[no].answers[j].answer+'">' + questions[no].answers[j].option + '</button>';
         text += pilihanJawaban  + "<br>";
         document.getElementById("demo").innerHTML = text;
     }
@@ -85,51 +85,54 @@ function next() {
     if (no < questions.length - 1) {
         no += 1;
         document.getElementById("pertanyaan").innerHTML = questions[no].question;
-        document.getElementById("nomer").innerHTML = no;
-
+        // document.getElementById("nomer").innerHTML = no;
        
     let text = "";
    
         for(let j=0; j<4; j++){
-            let pilihanJawaban = '<button onClick="cekJawaban('+j+')" id="pilihan'+ j +'" value="'+ questions[no].answers[j].answer+'">' + questions[no].answers[j].option + '</button>';
+            let pilihanJawaban = '<button class="w-100 p-3 btn-pil" onClick="cekJawaban('+j+')" id="pilihan'+ j +'" value="'+ questions[no].answers[j].answer+'">' + questions[no].answers[j].option + '</button>';
             text += pilihanJawaban  + "<br>";
             document.getElementById("demo").innerHTML = text;
         }    
+    }else if(no == questions.length - 1){
+        setTimeout(totalScore, 3000);
+        var element = document.getElementById("soal");
+        element.classList.add("d-none");
     }
 
   
 }
 
-function prev() {
-    if (no > 0) {
-        no -= 1;
-        document.getElementById("pertanyaan").innerHTML = questions[no].question;
-        document.getElementById("nomer").innerHTML = no;
-        
-            
-    let text = "";
-   
-        for(let j=0; j<4; j++){
-            let pilihanJawaban = '<button onClick="cekJawaban('+j+')" id="pilihan'+ j +'" value="'+ questions[no].answers[j].answer  +'">' + questions[no].answers[j].option + '</button>';
-            text += pilihanJawaban  + "<br>";
-            document.getElementById("demo").innerHTML = text;
-        }   
-    }
-}
-
-
+let score = [];
 let nilai = 0;
 function cekJawaban(i) {
+
         var jwb = document.getElementById('pilihan'+i).value;
+        next();
         if(jwb == 'true'){
             nilai +=10;
-            console.log(nilai);
+            console.log(10);
+            score.push(10);
             return nilai;
         }else if(jwb == 'false'){
-            nilai -= 10;
+            nilai += 0;
             return nilai;
         }
 }
+
+function totalScore(){
+    let sum = score.reduce(function(a, b){
+        return a + b;
+    });
+      
+      console.log(sum);
+}
+
+
+
+
+
+
 
 
     
